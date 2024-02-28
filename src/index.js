276ec8cbf91c6cfc9fdd710d0c09967a941ebfd1
leoshliak <levshlyak@gmail.com>
2024-02-28 17:49:36 +0200

    design is almost finished

--- src/index.js
+++ src/index.js
@@ -1,19 +1,60 @@
 import _ from 'lodash';
 import './style.css';
 
- const activeLink = document.querySelector('.active');
-const navLinks = document.querySelectorAll('.list-link');
 
-/*navLinks.forEach(addEventListener('click', (e)=>{
-activeLink.classList.remove('active');
- e.target.classList.add('active');
- e.target.preventDefault();
-}));*/
+const listLinks = document.querySelectorAll('.list-link');
+const addBtn = document.querySelector('.add-button');
+const addDialog = document.querySelector('.add-dialog');
+const addDialogCloseBtn = document.querySelector('.dialog-close');
+const lowPriorityBtn = document.querySelector('.low-priority-pick');
+const midPriorityBtn = document.querySelector('.medium-priority-pick');
+const highPriorityBtn = document.querySelector('.high-priority-pick');
 
-navLinks.forEach(link => {
+  listLinks.forEach(link => {
     link.addEventListener('click', (e) => {
-       e.preventDefault();
-      navLinks.forEach(link => link.classList.remove('active'));
+      listLinks.forEach(link => link.classList.remove('active'));
       e.target.classList.add('active');
     });
   });
+
+
+  addBtn.addEventListener('click', () =>{
+    addDialog.showModal();
+  });
+
+
+  addDialogCloseBtn.addEventListener('click', () => {
+    addDialog.classList.add('hide');
+    addDialog.addEventListener('animationend', onAnimationEnd, false);
+  });
+  
+  function onAnimationEnd() {
+    addDialog.classList.remove('hide');
+    addDialog.close();
+  
+    addDialog.removeEventListener('animationend', onAnimationEnd, false);
+  }
+
+ lowPriorityBtn.addEventListener('click', (e) =>{
+   e.target.classList.add('low-picked');
+   midPriorityBtn.classList.remove('mid-picked');
+   highPriorityBtn.classList.remove('high-picked');
+ });
+
+midPriorityBtn.addEventListener('click', (e) =>{
+  e.target.classList.add('mid-picked');
+  lowPriorityBtn.classList.remove('low-picked');
+  highPriorityBtn.classList.remove('high-picked');
+});
+
+highPriorityBtn.addEventListener('click', (e) =>{
+ e.target.classList.add('high-picked');
+ midPriorityBtn.classList.remove('low-picked');
+ midPriorityBtn.classList.remove('mid-picked');
+});
+
+
+   
+      
+          
+     
